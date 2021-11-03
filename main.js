bf5962b308bc5d3952f1ab056c49e104a670c161
Cristhian Recalde <89264838+Isnotcristhianr@users.noreply.github.com>
2021-11-03 16:16:47 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+//javascript
+
+//menu despegable
+document.querySelector('.menu-btn').addEventListener('click', () => {
+    document.querySelector('.nav-menu').classList.toggle('show');
+})
+
+//efecto desplazar
+ScrollReveal().reveal('.showcase');
+ScrollReveal().reveal('.Showcase', { delay: 500 });
+ScrollReveal().reveal('.news-cards', { delay: 500 });
+ScrollReveal().reveal('.news-cards', { delay: 500 });
+ScrollReveal().reveal('.cards-banner-one', { delay: 500 });
+ScrollReveal().reveal('.cards-banner-one', { delay: 500 });
+ScrollReveal().reveal('.social', { delay: 500 });
+ScrollReveal().reveal('.social', { delay: 500 });
+
+//ir arriba
+
